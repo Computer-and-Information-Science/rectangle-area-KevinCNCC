e5c7d6e232f0d4fc3d13df7b93b11aab693c3ca4
KevinCNCC <kevin.crook@student.northampton.edu>
2026-01-28 19:43:39 +0000

    Calculated Area and Perimeter, then output the properties of the rectangle.

--- main.cpp
+++ main.cpp
@@ -4,6 +4,12 @@ using namespace std;
 int main() {
   const double length = 7.2;
   const double width = 1.3;
-
-
+  double area, perimeter;
+  area = length*width;
+  perimeter = (length*2) + (width*2);
+  cout << "Rectangle Proterties:" << endl;
+  cout << "Length = " << length << endl;
+  cout << "Width = " << width << endl;
+  cout << "Area = " << area << endl;
+  cout << "Perimeter = " << perimeter << endl;
 }
